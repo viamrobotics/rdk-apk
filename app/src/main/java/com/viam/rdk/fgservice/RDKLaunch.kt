4ec20ff98c09096f60893a8cdf84f96ed8608571
Abe Winter <abe-winter@users.noreply.github.com>
2024-03-01 14:17:22 -0500

    basic compose ui

--- app/src/main/java/com/viam/rdk/fgservice/RDKLaunch.kt
+++ app/src/main/java/com/viam/rdk/fgservice/RDKLaunch.kt
@@ -3,16 +3,41 @@ package com.viam.rdk.fgservice
 import android.app.Activity
 import android.content.Intent
 import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+//import com.viam.rdk.fgservice.ui.theme.RDKForegroundTheme
 
 private const val TAG = "RDKLaunch"
 
 // todo: disable lint-baseline.xml entries related to API 28 + fix
-class RDKLaunch : Activity(){
+class RDKLaunch : ComponentActivity(){
     override fun onStart() {
         super.onStart()
-        // todo: prompt for necessary permissions first
-        startForegroundService(Intent(this, RDKForegroundService::class.java))
+        startService(Intent(this, RDKForegroundService::class.java))
         Log.i(TAG, "started RDK service")
-        finishAndRemoveTask()
+        setContent {
+            Greeting("Android")
+        }
     }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Greeting("Android")
 }
